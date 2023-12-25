104b6d8fc2b1bc065020d6691b437819e75e4a29
Hardy Rafael Jimenez Matos <hardy.rafel17@gmail.com>
2023-12-25 23:11:31 +0100

    update

--- init.lua
+++ init.lua
@@ -1,2 +1,10 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+if vim.g.vscode then
+  -- VSCode extension
+  require("vscode.lazy")
+  print("VSCode extension")
+else
+  -- ordinary Neovim
+  --
+  -- bootstrap lazy.nvim, LazyVim and your plugins
+  require("config.lazy")
+end
